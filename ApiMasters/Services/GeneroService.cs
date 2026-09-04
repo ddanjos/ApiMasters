65844bb85f4bfb49306f335e0a78c141effe1654
ddanjos <ddanjos.dev@gmail.com>
2026-09-03 21:32:33 -0300

    Adicao da camada de service para o genero

---
+++ ApiMasters/Services/GeneroService.cs
@@ -0,0 +1,58 @@
+using ApiMasters.Data.Repositories;
+using ApiMasters.Models;
+
+namespace ApiMasters.Services;
+
+public class GeneroService : IGeneroService
+{
+    private readonly   IGeneroRepository _repository;
+    public GeneroService(IGeneroRepository repository)
+    {
+        _repository = repository;
+    }
+
+
+    public async Task<List<Genero>> ObterTodosAsync()
+    {
+        return await _repository.ObterTodosAsync();
+    }
+
+    public async Task<Genero?> ObterPorIdAsync(int id)
+    {
+        return await _repository.ObterPorIdAsync(id);
+    }
+
+    public Task<Genero?> ObterPorNomeAsync(string nome)
+    {
+        return  _repository.ObterPorNomeAsync(nome);
+    }
+
+    public async Task<Genero> CriarAsync(Genero genero)
+    {
+        _repository.Criar(genero);
+        await  _repository.SalvarAlteracoesAsync();
+        return genero;
+    }
+
+    public async Task<bool> AtualizarAsync(int id, Genero generoAjustado)
+    {
+        var existente = await _repository.ObterPorIdAsync(id);
+        if (existente is null) return false;
+
+        existente.Nome =  generoAjustado.Nome;
+                
+        _repository.Atualizar(existente);
+        await _repository.SalvarAlteracoesAsync();
+        return true;
+    }
+
+    public async Task<bool> DeletarAsync(int id)
+    {
+        var existente = await _repository.ObterPorIdAsync(id);
+        if (existente is null) return false;
+        
+        _repository.Deletar(existente);
+        await _repository.SalvarAlteracoesAsync();
+        return true;
+    }
+}
---
+++ ApiMasters/Services/IGeneroService.cs
@@ -0,0 +1,13 @@
+using ApiMasters.Models;
+
+namespace ApiMasters.Services;
+
+public interface IGeneroService
+{
+    Task<List<Genero>> ObterTodosAsync();
+    Task<Genero?> ObterPorIdAsync(int id);
+    Task<Genero?> ObterPorNomeAsync(string nome);
+    Task<Genero> CriarAsync(Genero genero);
+    Task<bool> AtualizarAsync(int id, Genero generoAjustado);
+    Task<bool> DeletarAsync(int id);
+}
