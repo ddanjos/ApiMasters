3b2a9afecb50338e110af753ce0adc236f00d85a
ddanjos <ddanjos.dev@gmail.com>
2026-09-06 22:52:05 -0300

    Adicao dos dtos de criacao e resposta para a musica

---
+++ ApiMasters/DTOs/MusicaDTOs.cs
@@ -0,0 +1,23 @@
+using ApiMasters.Models;
+
+namespace ApiMasters.DTOs;
+
+public class MusicaCriacaoDto
+{
+    public string Nome { get; set; } = string.Empty;
+    public int GeneroId { get; set; } 
+    public int ArtistaId { get; set; }
+    public int Duracao {get; set;}
+    public List<int> GenerosIds { get; set; } =  new();
+
+}
+
+public class MusicaRespostaDto
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public int ArtistaId  { get; set; }
+    public List<GeneroRespostaDto> Generos { get; set; }
+    
+    public int Duracao { get; set; }
+}
