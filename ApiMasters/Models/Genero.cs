6c4b9894d9a7f156230379be828b96f1988d5585
ddanjos <ddanjos.dev@gmail.com>
2026-09-06 22:52:19 -0300

    AJUSTES na entidade

--- ApiMasters/Models/Genero.cs
+++ ApiMasters/Models/Genero.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Nome { get; set; } = string.Empty;
+        
         public ICollection<Musica> Musicas { get; set; } = new List<Musica>();
     }
 }
