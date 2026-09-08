ad02fd2a6fed5b00c2e4690842926b86133f4a1d
Davidy <163132668+ddanjos@users.noreply.github.com>
2026-09-08 07:46:49 -0300

    Add DTO classes for Genero creation and alteration
    faltou o push e tive que fazer na mão xd :_

---
+++ ApiMasters/DTOs/GeneroDto.cs
@@ -0,0 +1,15 @@
+public class GeneroRespostaDto
+{           
+  public int Id {get; set;}
+  public string Nome {get;set} = string.empty;
+}  
+
+public class GeneroCriacaoDto
+{
+  public string Nome = string.empty;
+}
+
+public class GeneroAlteracaoDto
+{
+  public string Nome {get; set;}
+}
