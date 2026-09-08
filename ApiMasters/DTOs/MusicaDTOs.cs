dec4db05cdea852397b1c4297c4f094391dbfe08
ddanjos <ddanjos.dev@gmail.com>
2026-09-07 23:34:13 -0300

    Ajustes no dto para corpo de atualização nas requsicoes

--- ApiMasters/DTOs/MusicaDTOs.cs
+++ ApiMasters/DTOs/MusicaDTOs.cs
@@ -5,7 +5,6 @@ namespace ApiMasters.DTOs;
 public class MusicaCriacaoDto
 {
     public string Nome { get; set; } = string.Empty;
-    public int GeneroId { get; set; } 
     public int ArtistaId { get; set; }
     public int Duracao {get; set;}
     public List<int> GenerosIds { get; set; } =  new();
@@ -17,7 +16,7 @@ public class MusicaRespostaDto
     public int Id { get; set; }
     public string Nome { get; set; } = string.Empty;
     public int ArtistaId  { get; set; }
-    public List<GeneroRespostaDto> Generos { get; set; }
+    public List<GeneroRespostaDto> Generos { get; set; } = new();
     
     public int Duracao { get; set; }
 }
